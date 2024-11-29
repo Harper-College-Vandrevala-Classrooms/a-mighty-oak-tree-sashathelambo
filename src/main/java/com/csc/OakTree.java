445bb348a5f9715bdfebe3c1f934b685515c7acb
sashathelambo <166897058+sashathelambo@users.noreply.github.com>
2024-11-29 12:16:22 -0600

    up

--- src/main/java/com/csc/OakTree.java
+++ src/main/java/com/csc/OakTree.java
@@ -1,7 +1,47 @@
 package com.csc;
 
 public class OakTree {
+  private Squirrel squirrel;
+  private OakTree left;
+  private OakTree right;
+
+  public OakTree(Squirrel squirrel) {
+    this.squirrel = squirrel;
+    this.left = null;
+    this.right = null;
+  }
+
+  public void attachLeft(OakTree node) {
+    this.left = node;
+  }
+
+  public void attachRight(OakTree node) {
+    this.right = node;
+  }
+
+  public OakTree left() {
+    return this.left;
+  }
+
+  public OakTree right() {
+    return this.right;
+  }
+
+  public Squirrel getSquirrel() {
+    return this.squirrel;
+  }
+
   public static void main(String[] args) {
-    System.out.println("Oak Tree!!!");
+    // Example usage:
+    Squirrel rocky = new Squirrel("Rocky");
+    Squirrel bullwinkle = new Squirrel("Bullwinkle");
+    Squirrel sandy = new Squirrel("Sandy");
+
+    OakTree root = new OakTree(rocky);
+    OakTree leftNode = new OakTree(bullwinkle);
+    OakTree rightNode = new OakTree(sandy);
+
+    root.attachLeft(leftNode);
+    root.attachRight(rightNode);
   }
 }
--- src/test/java/com/csc/TestOakTree.java
+++ src/test/java/com/csc/TestOakTree.java
@@ -1,21 +1,71 @@
 package com.csc;
 
-import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.*;
 
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 
 public class TestOakTree {
+    private OakTree root;
+    private Squirrel rocky;
+    private Squirrel bullwinkle;
+    private Squirrel sandy;
 
-  OakTree tree;
+    @BeforeEach
+    void setUp() {
+        rocky = new Squirrel("Rocky");
+        bullwinkle = new Squirrel("Bullwinkle");
+        sandy = new Squirrel("Sandy");
+        root = new OakTree(rocky);
+    }
 
-  @BeforeEach
-  void setUp() {
-    tree = new OakTree();
-  }
+    @Test
+    void testCreateNode() {
+        assertNotNull(root);
+        assertEquals("Rocky", root.getSquirrel().getName());
+    }
 
-  @Test
-  void itWorks() {
-    assertEquals(true, true);
-  }
+    @Test
+    void testAttachLeft() {
+        OakTree leftNode = new OakTree(bullwinkle);
+        root.attachLeft(leftNode);
+        
+        assertNotNull(root.left());
+        assertEquals("Bullwinkle", root.left().getSquirrel().getName());
+    }
+
+    @Test
+    void testAttachRight() {
+        OakTree rightNode = new OakTree(sandy);
+        root.attachRight(rightNode);
+        
+        assertNotNull(root.right());
+        assertEquals("Sandy", root.right().getSquirrel().getName());
+    }
+
+    @Test
+    void testCompleteTree() {
+        OakTree leftNode = new OakTree(bullwinkle);
+        OakTree rightNode = new OakTree(sandy);
+        
+        root.attachLeft(leftNode);
+        root.attachRight(rightNode);
+        
+        // Test root
+        assertEquals("Rocky", root.getSquirrel().getName());
+        
+        // Test left subtree
+        assertNotNull(root.left());
+        assertEquals("Bullwinkle", root.left().getSquirrel().getName());
+        
+        // Test right subtree
+        assertNotNull(root.right());
+        assertEquals("Sandy", root.right().getSquirrel().getName());
+    }
+
+    @Test
+    void testInitiallyNullChildren() {
+        assertNull(root.left());
+        assertNull(root.right());
+    }
 }
